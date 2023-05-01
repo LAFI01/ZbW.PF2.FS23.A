2d6f491d70c4a48a71fe4163338f4b1a2388d3b0
Larissa <Larissa@DESKTOP-N0IML8E>
2023-05-01 19:52:59 +0200

    Add project files.

---
+++ Einheit11/Adressverwaltung.cs
@@ -0,0 +1,20 @@
+namespace Einheit11
+{
+  public partial class Adressverwaltung : Form
+  {
+    public Adressverwaltung()
+    {
+      InitializeComponent();
+    }
+
+    private void Adressverwaltung_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private void label1_Click(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
